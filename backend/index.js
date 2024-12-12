d7cd8aaca5bcf47f3c22d8378b53e275855d948a
Saikat <saikatchakraborty416@gmail.com>
2024-12-12 08:22:43 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,14 @@ const app=express();
 app.use(express.json());
 require("dotenv").config();
 const cors=require('cors');
-const PORT=process.env.PORT;
+const PORT=process.env.PORT||3000;
 const cookieParser = require('cookie-parser');
 const user = require("./routes/user");
 
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://note-x-pi.vercel.app/',
+    // origin: 'https://note-x-pi.vercel.app/',
+    origin: 'http://localhost:3001',
     credentials: true
   }));
 
