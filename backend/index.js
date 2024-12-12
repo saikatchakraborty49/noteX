b285e109eb94a2485e2f90eee00c4410983e2f4a
Saikat <saikatchakraborty416@gmail.com>
2024-12-12 09:05:02 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 //     credentials: true
 //   }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://notex-backend-0j9r.onrender.com/");
+  res.header("Access-Control-Allow-Origin", "https://note-x-pi.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
