dd314e7ced03463905e05be507970ab18778a976
Saikat <saikatchakraborty416@gmail.com>
2025-02-07 04:53:52 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -14,16 +14,16 @@ app.use(cors({
     // credentials: true
   }));
 // const allowedOrigins = ["https://note-x-pi.vercel.app"]; // Add more origins if needed
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 
 //route import and mount
